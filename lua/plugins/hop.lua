45ed075a8034f71a7b7edcc83852e91bd8e806e3
Takumi Tosaki <takumi.tosaki@giftee.co>
2025-05-27 09:22:23 +0900

    mod hop keys

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -8,6 +8,8 @@ return {
   end,
   keys = {
     { mode = "", "f", "<Cmd>HopChar2<CR>", desc = "Hop 2 char" },
-    { mode = "", "s", "<Cmd>HopChar1CurrentLine<CR>", desc = "Hop 1 char in current line" },
+    { mode = "", "F", "<Cmd>HopWord<CR>", desc = "Hop word" },
+    { mode = "", "s", "<Cmd>HopChar1CurrentLineAC<CR>", desc = "Hop 1 char in current line" },
+    { mode = "", "S", "<Cmd>HopChar1CurrentLineBC<CR>", desc = "Hop 1 char in current line" },
   }
 }
